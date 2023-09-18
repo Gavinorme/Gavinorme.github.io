e55138e8a1b28825d788a0b52d3fdbde8710b570
Gavinorme <Gorme@email.sc.edu>
2023-09-17 20:03:33 -0400

    assign6

---
+++ Inclasspractice/sept14/script.js
@@ -0,0 +1,44 @@
+//console.log("Gavin")
+//const add = (a,b) => a+b;
+
+//const add = (a,b) => { return a + b; }
+
+//console.log(add(5,6));
+
+// const square = a => a*a;
+
+// console.log(square(5));
+
+// const hello = () => console.log("Hello and dang it desmond");
+// hello();
+
+// const helloSpecific = userName => console.log("Hello " + userName + "!");
+
+// helloSpecific("Gavin");
+
+// const helloFullName = (firstName, lastName) => {
+//     console.log("Hello "+firstName+" "+lastName);
+//     console.log("Desmond is mid");
+// };
+
+// helloFullName("Gavin", "Orme");
+
+//Not ok to change a constant
+// const myName = "Gavin";
+// myName = "sally";
+
+//const myFunct = () => console.log("Funk");
+
+const moveSqaure = () => {
+    document.getElementById("square").classList.add("move-square");
+}
+
+const displayName = () => {
+    const firstName = document.getElementById("txt-first-name").value;
+    console.log("Hello "+firstName+"!");
+}
+
+window.onload = () => {
+    document.getElementById("button-move").onclick = moveSqaure;
+    document.getElementById("button-show-name").onclick = displayName;
+    }
--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -6,7 +6,31 @@ const hideHead = () => {
     document.getElementById("head").classList.add("hide");
 }
 
+const moveLion = () => {
+    document.getElementById("lion").classList.add("move-lion");
+}
+
+const addComment = () => {
+    const productName = document.getElementById("txt-product-name").value;
+    const productP = document.getElementById("product-message");
+    const ratingNum = document.getElementById("txt-rating").value;
+    const ratingR = document.getElementById("rating");
+    const comment = document.getElementById("txt-comment").value;
+    const commentC = document.getElementById("comment");
+    const userName = document.getElementById("txt-user-name").value;
+    const userN = document.getElementById("user-message");
+
+    productP.innerHTML = (productName);
+    ratingR.innerHTML = ("Rating: "+ratingNum + "/5 stars ");
+    commentC.innerHTML = ("Description: "+comment+". ");
+    userN.innerHTML = ("By: " + userName);
+    
+}
+
+
 window.onload = () => {
     document.getElementById("button-show").onclick = showHead;
     document.getElementById("button-hide").onclick = hideHead;
+    document.getElementById("button-dance").onclick = moveLion;
+    document.getElementById("button-input").onclick = addComment;
     }
