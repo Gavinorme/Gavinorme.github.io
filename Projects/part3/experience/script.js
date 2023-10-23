4a93efcf2142308bb62dfed3841e1bf194d3da24
Gavinorme <Gorme@email.sc.edu>
2023-10-23 12:30:05 -0400

    forms

--- Projects/part3/experience/script.js
+++ Projects/part3/experience/script.js
@@ -3,10 +3,15 @@ const toggleNav = () =>
     document.getElementById("nav-items").classList.toggle("hide-small");
 };
 
-const showExp = () =>
+const show = () =>
 {
     document.getElementById("addExp").classList.remove("hide");
     document.getElementById("p").classList.remove("hide");
+    document.getElementById("button-add").classList.remove("hidden");
+}
+
+const showExp = () =>
+{
     const resultDiv = document.getElementById("result");
     const name1 = document.getElementById("txt-name").value;
     const description = document.getElementById("txt-description").value;
@@ -15,6 +20,8 @@ const showExp = () =>
 
     const section = document.createElement("section");
     resultDiv.append(section);
+    const h1 = document.createElement("h1");
+    section.append(h1);
     const h3 = document.createElement("h3");
     section.append(h3);
     const p = document.createElement("p");
@@ -24,6 +31,7 @@ const showExp = () =>
     const p2 = document.createElement("p");
     section.append(p2);
 
+    h1.textContent = `Your form has been submitted successfully`;
     h3.textContent = `${name1}`;
     p.textContent = `${description}`;
     p1.textContent = `${skills}`;
@@ -32,46 +40,10 @@ const showExp = () =>
     document.getElementById("button-add").classList.remove("hidden");
 }
 
-const showEmailResult = async (e) => {
-    e.preventDefault();
-    const result = document.getElementById("result");
-    let response = await getEmailResult();
-    if (response.status == 200) {
-      result.innerHTML = "Email Successfully Sent";
-    } else {
-      result.innerHTML = "Sorry, your email was not sent.";
-    }
-  };
-  
-  const getEmailResult = async (e) => {
-    const form = document.getElementById("contact-form");
-    const formData = new FormData(form);
-    const object = Object.fromEntries(formData);
-    const json = JSON.stringify(object);
-    const result = document.getElementById("result");
-    result.innerHTML = "Please wait...";
-  
-    try {
-      const response = await fetch("https://api.web3forms.com/submit", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: json,
-      });
-      return response;
-    } catch (error) {
-      console.log(error);
-      document.getElementById("result").innerHTML =
-        "Sorry your email couldn't be sent";
-    }
-  };
-
 
 window.onload = () => 
 {
     document.getElementById("hamburger").onclick = toggleNav;
-    document.getElementById("button-login").onclick = showExp;
+    document.getElementById("button-login").onclick = show;
     document.getElementById("button-add").onclick = showExp;
 }
