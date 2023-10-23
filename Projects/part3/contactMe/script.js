86f93f240a7fd8383e800f4bd6befc33c5533755
Gavinorme <Gorme@email.sc.edu>
2023-10-23 13:20:45 -0400

    forms

--- Projects/part3/contactMe/script.js
+++ Projects/part3/contactMe/script.js
@@ -29,13 +29,47 @@ const contact = () =>
     p2.textContent = `${"Contact Reason: " +contact}`;
 
     document.getElementById("goodbye").classList.remove("hidden");
-
 }
 
-
+const showEmailResult = async (e) => {
+    e.preventDefault();
+    const result = document.getElementById("result");
+    let response = await getEmailResult();
+    if (response.status == 200) {
+      result.innerHTML = "Email Successfully Sent. Thank You!";
+    } else {
+      result.innerHTML = "Sorry, your email was not sent.";
+    }
+  };
+  
+  const getEmailResult = async (e) => {
+    const form = document.getElementById("contact-form");
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const result = document.getElementById("result");
+    result.innerHTML = "Please wait...";
+  
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: json,
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+      document.getElementById("result").innerHTML =
+        "Sorry your email couldn't be sent";
+    }
+  };
 
 window.onload = () => 
 {
     document.getElementById("hamburger").onclick = toggleNav;
     document.getElementById("button-contact").onsubmit = contact;
+    document.getElementById("contact-form").onsubmit = showEmailResult;
 }
