891644116c9c1bc4e745498cb2cd4fc883c7a093
Gavinorme <Gorme@email.sc.edu>
2023-10-28 18:19:30 -0400

    JSON

--- Projects/part4/part3/projects/script.js
+++ Projects/part4/part3/projects/script.js
@@ -3,8 +3,45 @@ const toggleNav = () =>
     document.getElementById("nav-items").classList.toggle("hide-small");
 };
 
+const getProjects = async () => {
+    const url = "https://Gavinorme.github.io/Projects/part4/part3/projects/projects.json";
+
+    try {
+        const reponse = await fetch(url);
+        return await reponse.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const showProjects  = async () => {
+    let projects = await getProjects();
+    let projectsSection = document.getElementById("projects");
+
+    projects.forEach((project) => {
+        projectsSection.append(getProjectItem(project));
+    });
+};
+
+const getProjectItem = (project) => 
+{
+    let section = document.createElement("section");
+    section.classList.add("project");
+    let h1 = document.createElement("img");
+    let url = "https://Gavinorme.github.io/json/";
+    h1.src = url+project.img;
+    section.append(h1);
+
+    let ul = document.createElement("ul");
+    section.append(ul);
+    ul.append(getLi(project.title));
+
+
+    return section;
+};
 
 window.onload = () => 
 {
     document.getElementById("hamburger").onclick = toggleNav;
+    showProjects();
 }
